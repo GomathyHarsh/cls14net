53f1b6a5f5310d0c8cf8eb236e50e9c00ae7815a
GomathyHarsh <110896778+GomathyHarsh@users.noreply.github.com>
2022-09-01 15:02:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//
+function foo(){
+    var res=document.getElementById("fn").value;
+    console.log(res);
+    var res1=document.getElementById("mn").value;
+    console.log(res1);
+    var res2=document.getElementById("ln").value;
+    console.log(res2);
+    var res3=document.getElementById("e").value;
+    console.log(res3);
+
+
+}
